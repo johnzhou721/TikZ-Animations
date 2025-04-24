c1028568720b19441ff6054099f6d66cec2a0e00
Jasper <twilliam1925@outlook.com>
2025-04-24 00:58:57 -0600

    small edit to  gif

--- Sources/elliptic_spherical_mobius_transformation/main.py
+++ Sources/elliptic_spherical_mobius_transformation/main.py
@@ -5,20 +5,26 @@ from Animatetex import animatetex
 iterations = 24
 def main():
   """
+  This code animates a
   """
   animatetex.before_loop()
-  for angle in np.linspace(0,10,iterations):
-      with open(animatetex.TeX_file, 'w') as latex:
-          latex.write(
-              '\\documentclass{beamer}\n' +
-              '\\beamertemplatenavigationsymbolsempty\n'
-              '\\usepackage{Sources/elliptic_spherical_mobius_transformation/preamble}\n' +
-              '\\directlua{rotation = ' + f'{angle}' + '}\n' +
-              '\\begin{document}\n' +
-              '\\input{Sources/elliptic_spherical_mobius_transformation/document}\n' +
-              '\\end{document}'
-          )
-      animatetex.during_loop()
+  start_angle = 0
+  end_angle = 10
+  samples_angle = iterations + 1
+  # np.linspace handles the step
+  for angle in np.linspace(0,10,samples_angle):
+      if angle < end_angle:
+        with open(animatetex.TeX_file, 'w') as latex:
+            latex.write(
+                '\\documentclass{beamer}\n' +
+                '\\beamertemplatenavigationsymbolsempty\n'
+                '\\usepackage{Sources/elliptic_spherical_mobius_transformation/preamble}\n' +
+                '\\directlua{rotation = ' + f'{angle}' + '}\n' +
+                '\\begin{document}\n' +
+                '\\input{Sources/elliptic_spherical_mobius_transformation/document}\n' +
+                '\\end{document}'
+            )
+        animatetex.during_loop()
   animatetex.after_loop()
 if __name__ == "__main__":
     main()
