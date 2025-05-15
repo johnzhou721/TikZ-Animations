f4068338507d6695c3beaffa455f7880669f1a2c
Jasper <twilliam1925@outlook.com>
2025-05-15 16:20:56 -0600

    math documentation

--- Packages/pline-vec/math.lua
+++ Packages/pline-vec/math.lua
@@ -107,6 +107,54 @@ function pv_ZYZ_rotation_matrix(angles,vector)
     return result
 end
 
+function pv_ZYZ_rotation_matrix_x(angles,vector)
+    local c1 = math.cosd(angles[1])
+    local c2 = math.cosd(angles[2])
+    local c3 = math.cosd(angles[3])
+    local s1 = math.sind(angles[1])
+    local s2 = math.sind(angles[2])
+    local s3 = math.sind(angles[3])
+    local x = (
+        (c1*c2*c3-s1*s3)*vector[1] +
+        (-c1*c2*s3-s1*c3)*vector[2] +
+        c1*s2*vector[3]
+    )
+    local result = x
+    return result
+end
+
+function pv_ZYZ_rotation_matrix_y(angles,vector)
+    local c1 = math.cosd(angles[1])
+    local c2 = math.cosd(angles[2])
+    local c3 = math.cosd(angles[3])
+    local s1 = math.sind(angles[1])
+    local s2 = math.sind(angles[2])
+    local s3 = math.sind(angles[3])
+    local y = (
+        (s1*c2*c3+c1*s3)*vector[1] +
+        (-s1*c2*s3+c1*c3)*vector[2] +
+        s1*s2*vector[3]
+    )
+    local result = y
+    return result
+end
+
+function pv_ZYZ_rotation_matrix_z(angles,vector)
+    local c1 = math.cosd(angles[1])
+    local c2 = math.cosd(angles[2])
+    local c3 = math.cosd(angles[3])
+    local s1 = math.sind(angles[1])
+    local s2 = math.sind(angles[2])
+    local s3 = math.sind(angles[3])
+    local z = (
+        -s2*c3*vector[1] +
+        s2*s3*vector[2] +
+        c2*vector[3]
+    )
+    local result = z
+    return result
+end
+
 function pv_ZYZ_rotation_matrix_x_tex(angles,vector)
     local c1 = math.cosd(angles[1])
     local c2 = math.cosd(angles[2])
