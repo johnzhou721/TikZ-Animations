6399f4ae1572e30b5b586378f4880abb27197f14
Jasper <twilliam1925@outlook.com>
2025-05-17 15:16:20 -0600

    linalg.lua

--- MetaPost_ConTeXt_Lua/linalg.lua
+++ MetaPost_ConTeXt_Lua/linalg.lua
@@ -11,13 +11,70 @@ function la.add(A, B)
     local columns_B = #B[1]
     assert(rows_A == rows_B and columns_A == columns_B, "Wrong size matrices for addition.")
     local sum = {}
-    for row = 1, rows_A do
+    for row = 1, rows_A, 1 do
         sum[row] = {}
-        for column = 1, columns_A do
+        for column = 1, columns_A, 1 do
             sum[row][column] = A[row][column] + B[row][column]
         end
     end
     return sum
+end
+
+--[[
+    Matrix scalar multiplication
+]]
+function la.scale(a,A)
+    local rows_A = #A
+    local columns_A = #A[1]
+    local product = {}
+    for row = 1, rows_A, 1 do
+        product[row] = {}
+        for column = 1, columns_A, 1 do
+            product[row][column] = a * A[row][column]
+        end
+    end
+    return product
+end
+
+--[[
+    Matrix subtraction
+]]
+function la.sub(A,B)
+    local rows_A = #A
+    local columns_A = #A[1]
+    local rows_B = #B
+    local columns_B = #B[1]
+    assert(rows_A == rows_B and columns_A == columns_B, "Wrong size matrices for subtraction.")
+    local sum = {}
+    for row = 1, rows_A, 1 do
+        sum[row] = {}
+        for column = 1, columns_A, 1 do
+            sum[row][column] = A[row][column] + la.scale(-1,B[row][column])
+        end
+    end
+    return sum
+end
+
+--[[
+    Matrix multiplication
+]]
+function la.mult(A,B)
+    local rows_A = #A
+    local columns_A = #A[1]
+    local rows_B = #B
+    local columns_B = #B[1]
+    assert(columns_A == rows_B, "Wrong size matrices for multiplication.")
+    local product = {}
+    for row = 1, rows_A, 1 do
+        product[row] = {}
+        for column = 1, columns_B, 1 do
+            product[row][column] = 0
+            for dot_product_step = 1, columns_A, 1 do
+                product[row][column] = product[row][column] + A[row][dot_product_step] * B[dot_product_step][column]
+            end
+        end
+    end
+    return product
 end
 
 
