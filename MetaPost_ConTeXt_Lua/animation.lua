d2577671ed5a6841a6e7b11d37db0f5e3e4ff7c3
Jasper <twilliam1925@outlook.com>
2025-05-18 05:24:44 -0600

    first context animation (purely by matrix mult.)

--- MetaPost_ConTeXt_Lua/animation.lua
+++ MetaPost_ConTeXt_Lua/animation.lua
@@ -1,34 +1,42 @@
 require("MetaPost_ConTeXt_Lua.linalg")
 
-local segments = {}
+local longitude_paths = {}
 
-for longitude = 0, 2*la.pi, la.pi/18 do
+count0 = 0
+for longitude = 0, 2*la.pi, la.pi/4 do
+    count0 = count0 + 1
+    longitude_paths[count0] = {}
+    count = 0
     for latitude = 0, 2*la.pi, la.pi/18 do
-        table.insert(segments,la.sphere(longitude,latitude))
+        count = count + 1
+        longitude_paths[count0][count] = la.sphere(longitude,latitude)
     end
 end
+
 
 function make_picture()
     local alpha = la.pi/6
     local beta = la.pi/6
     local gamma
-    local transformed_segs
-    for gamma = 0, 2*la.pi, la.pi/18 do
-        transformed_segs = la.mult(segments,la.ZYZrotation3D(alpha,beta,gamma))
+    local transformed_paths
+    for gamma = 0, 2*la.pi, la.pi/36 do
         context.startMPpage()
-        context("draw (-5 cm,-5 cm) -- (5 cm,-5 cm) -- (5 cm,5 cm) -- (-5 cm,5 cm);")
-        for segment = 1, #transformed_segs - 1, 1 do
-            context(
-                string.format(
-                    [[
-                        draw (%f cm,%f cm) -- (%f cm,%f cm);
-                    ]]
-                    ,transformed_segs[segment][1]
-                    ,transformed_segs[segment][2]
-                    ,transformed_segs[segment+1][1]
-                    ,transformed_segs[segment+1][2]
+        for i = 1, #longitude_paths - 1, 1 do
+            transformed_paths = la.mult(longitude_paths[i],la.ZYZrotation3D(alpha,beta,gamma))
+            context("draw (-5 cm,-5 cm) -- (5 cm,-5 cm) -- (5 cm,5 cm) -- (-5 cm,5 cm);")
+            for segment = 1, #transformed_paths - 1, 1 do
+                context(
+                    string.format(
+                        [[
+                            draw (%f cm,%f cm) -- (%f cm,%f cm);
+                        ]]
+                        ,transformed_paths[segment][1]
+                        ,transformed_paths[segment][2]
+                        ,transformed_paths[segment+1][1]
+                        ,transformed_paths[segment+1][2]
+                    )
                 )
-            )
+            end
         end
         context.stopMPpage()
     end
