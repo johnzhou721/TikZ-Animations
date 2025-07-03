d07874470dc950dfc786b64a36907304d234dafc
Jasper <143286960+Pseudonym321@users.noreply.github.com>
2025-07-02 19:22:24 -0600

    append curve and render segments

--- tikz-3d/parametric.lua
+++ tikz-3d/parametric.lua
@@ -7,9 +7,9 @@ local observer_dir = { { 0, 0, -1, 1} }
 local observer_pos = { { 0, 0, 0, 1} }
 
 local function append_curve(hash)
-    local u_start      = tonumber(hash.u_start)
-    local u_stop       = tonumber(hash.u_stop)
-    local u_samples    = tonumber(hash.u_samples)
+    local u_start      = hash.u_start
+    local u_stop       = hash.u_stop
+    local u_samples    = hash.u_samples
     local x            = hash.x
     local y            = hash.y
     local z            = hash.z
@@ -23,23 +23,52 @@ local function append_curve(hash)
         return load(("return function(u) return %s end"):format(str))()
     end
 
-    local x = single_string_function(x)
-    local y = single_string_function(y)
-    local z = single_string_function(z)
+    local function single_string_expression(str)
+        if not str or str == "" then
+            return nil
+        end
+        return load(("return %s"):format(str))()
+    end
+
+    x = single_string_function(x)
+    y = single_string_function(y)
+    z = single_string_function(z)
+
+    u_start = single_string_expression(u_start)
+    u_stop = single_string_expression(u_stop)
 
     local u_step = (u_stop - u_start) / (u_samples - 1)
 
     local function parametric_curve(u)
-        return {x(u),y(u),z(u),1}
+        return { { x =  x(u), y = y(u), z = z(u), w = 1 } }
     end
 
     for i = 0, u_samples - 2 do
         local u = u_start + i * u_step
         local A = parametric_curve(u)
         local B = parametric_curve(u+u_step)
-        table.insert( segments, { {A,B}, draw_options, name } )
+        table.insert(segments, { segment = { A, B }, draw_options = draw_options, name = name })
     end
 end
+
+local function render_segments()
+    for _, segment in ipairs(segments) do
+        if #segment.segment == 2 then
+            local S, E = segment.segment[1], segment.segment[2]
+            local Sx, Sy = S[1].x, S[1].y
+            local Ex, Ey = E[1].x, E[1].y
+            local options = segment.draw_options
+            tex.sprint(
+                string.format(
+                    "\\draw[%s] (%f,%f) -- (%f,%f);"
+                    ,options,Sx, Sy, Ex, Ey
+                )
+            )
+        end
+    end
+    segments = {}
+end
+
 
 rtc.register_tex_cmd(
     "appendcurve", function()
@@ -55,3 +84,5 @@ rtc.register_tex_cmd(
     } end,
     { }
 )
+
+rtc.register_tex_cmd("rendersegments", function() render_segments() end, { })
